1bc5a2af37911cc8335a3e3ef4b20f4957ac577e
Plunser Fabio <fabio.plunser@student.uibk.ac.at>
2023-04-17 08:30:11 +0200

    fixed lock unlock accessPoint

--- frontend/src/routes/(app)/admin/accesspoints/+page.server.ts
+++ frontend/src/routes/(app)/admin/accesspoints/+page.server.ts
@@ -4,8 +4,34 @@ export async function load({ fetch }) {
   let res = await fetch(`http://${BACKEND_URL}/get-access-points`);
   res = await res.json();
 
-  console.log("access points", res);
+  // console.log("access points", res);
   return {
     accessPoints: res?.items || [],
   };
 }
+
+export const actions = {
+  unlock: async ({ cookies, request, fetch }) => {
+    const formData = await request.formData();
+
+    const res = await fetch(
+      `http://${BACKEND_URL}/set-unlocked-access-point?accessPointId=${formData.get(
+        "accessPointId"
+      )}&unlocked=${formData.get("unlocked")}`,
+      {
+        method: "POST",
+      }
+    );
+    const data = await res.json();
+    return {
+      status: res.status,
+      body: JSON.stringify(data),
+    };
+  },
+  delete: async ({ cookies, request, fetch }) => {
+    console.log("delete");
+  },
+  search: async ({ cookies, request, fetch }) => {
+    console.log("search");
+  },
+} satisfies Actions;
