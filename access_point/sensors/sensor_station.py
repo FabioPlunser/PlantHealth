7d53ef43fa2eb19cfd68bd14ccf2ac68b28455c9
Fabian Herbert Margreiter <fabian.margreiter@student.uibk.ac.at>
2023-04-21 17:10:57 +0200

    Fixed typo in get_short_uuid()

--- access_point/sensors/sensor_station.py
+++ access_point/sensors/sensor_station.py
@@ -40,7 +40,7 @@ def get_short_uuid(uuid: str) -> str:
     """
     Extracts the relevant 4 half-bytes from the 32 digit UUID to identify a characteristic type.
     """
-    if len(uuid) == 32:
+    if len(uuid.replace('-','')) == 32:
         return uuid[4:8]
     else:
         return uuid
