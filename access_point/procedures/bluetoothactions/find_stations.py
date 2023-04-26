f46ff268d84ffa87130065b084d1ee2e596cbfe9
Fabian Herbert Margreiter <fabian.margreiter@student.uibk.ac.at>
2023-04-26 17:29:59 +0200

    164: Split up procedure for finding stations into more steps

--- access_point/procedures/bluetoothactions/find_stations.py
+++ access_point/procedures/bluetoothactions/find_stations.py
@@ -3,6 +3,7 @@ import asyncio
 
 from datetime import timedelta
 from bleak import BleakClient
+from typing import Union
 
 from util import Config
 from server import Server, TokenDeclinedError
@@ -23,25 +24,50 @@ def find_stations(config: Config):
     All sensor stations for which that succeeds are then reported to the backend.
     """
     log.info('Starting to scan for sensor stations')
-    backend = Server(config.backend_address, config.token)
-    database = Database(DB_FILENAME)
 
     # start scan, ignore known stations
+    new_station_addresses = run_scan()
+
+    # get required data
+    report_data = get_data_from_new_stations(new_station_addresses)
+
+    # remove stations that have been enabled while scanning
+    report_data = clean_data(report_data)
+
+    # send data to backend
+    send_data_to_backend(config, report_data)
+
+    # disable scanning mode
+    config.update(scan_active=False)
+    log.info(f'Finished scan for new sensor stations')
+
+def run_scan() -> list[str]:
+    """
+    Runs the actual scan for sensor stations
+    :return: A list with the addresses of potential new sensor stations
+    """
+    database = Database(DB_FILENAME)
     try:
         known_addresses = database.get_all_known_sensor_station_addresses()
     except DatabaseError as e:
         log.error(f'Unable to load addresses of known sensor stations from database: {e}')
-        return
+        return []
     try:
         new_station_addresses = scan_for_new_stations(known_addresses, SENSOR_STATION_NAME, timedelta(seconds=10))
     except ConnectionError as e:
         log.error(f'Unable to scan for sensor stations: {e}')
-        return
+        return []
     log.info(f'Found {len(new_station_addresses)} potential new sensor stations')
+    return new_station_addresses
 
-    # get required data
+def get_data_from_new_stations(addresses: list[str]) -> list[dict[str, Union[str, int]]]:
+    """
+    Connects to the potential new sensor stations and tries to read the DIP switch id
+    :param addresses: The addresses of the sensor stations
+    :return: A list of dictionaries with addresses and dip-switch ids
+    """
     report_data = []
-    for address in new_station_addresses:
+    for address in addresses:
         try:
             dip_id = asyncio.run(get_dip_id(address))
             report_data.append({
@@ -50,16 +76,29 @@ def find_stations(config: Config):
             })
         except BLEConnectionError + (ReadError,) as e:
             log.warning(f'Unable to read DIP id from sensor station {address}: {e}')
+    return report_data
 
-    # remove stations that have been enabled while scanning
+def clean_data(report_data: list[dict[str, Union[str, int]]]):
+    """
+    Removes data of any sensor station from the report that has been enabled in the meantime
+    :param report_data: Output of get_data_from_new_stations()
+    :return: report_data with all these entries removed that belong to sensor stations that are enabled
+    """
+    database = Database(DB_FILENAME)
     try:
         known_addresses = database.get_all_known_sensor_station_addresses()
     except DatabaseError as e:
         log.error(f'Unable to load addresses of known sensor stations from database: {e}')
-        return   
-    report_data = [entry for entry in report_data if entry.get('address') not in known_addresses]
+        return []
+    return [entry for entry in report_data if entry.get('address') not in known_addresses]
 
-    # send data to backend
+def send_data_to_backend(config:Config, report_data: list[dict[str, Union[str, int]]]) -> None:
+    """
+    Sends the info on found sensor stations to the backend
+    :param config: The configuration containing backend address and token
+    :param report_data: Output from clean_data (or get_data_from_new_stations)
+    """
+    backend = Server(config.backend_address, config.token)
     if report_data:
         try:
             backend.report_found_sensor_station(report_data)
@@ -73,9 +112,6 @@ def find_stations(config: Config):
             return
     else:
         log.info('Did not find any new sensor stations to report to backend')
-
-    config.update(scan_active=False)
-    log.info(f'Disabled scanning mode')
 
 async def get_dip_id(address: str) -> int:
     """
