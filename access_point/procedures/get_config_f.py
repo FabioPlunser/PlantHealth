be6376cb69237e73b007ef2ddc2ef27910bc3ac7
Fabian Herbert Margreiter <fabian.margreiter@student.uibk.ac.at>
2023-04-28 18:43:19 +0200

    154: Simplified procedures.get_config()

--- access_point/procedures/get_config_f.py
+++ access_point/procedures/get_config_f.py
@@ -6,6 +6,42 @@ from util import Config
 
 log = logging.getLogger()
 
+def enable_sensor_stations(addresses: list[str]) -> None:
+    """Enables the sensor stations with the given addresses in the database"""
+    database = Database(DB_FILENAME)
+    if addresses: log.info(f'Enabling sensor stations: {addresses}')
+    for adr in addresses:
+        try:
+            database.enable_sensor_station(adr)
+        except DatabaseError as e:
+            log.error(f'Unable to enable sensor station {adr} in database: {e}')
+            continue
+
+def disable_sensor_stations(addresses: list[str]) -> None:
+    """Disables the sensor stations with the given addresses in the database"""
+    database = Database(DB_FILENAME)
+    if addresses: log.info(f'Disabling sensor stations: {addresses}')
+    for adr in addresses:
+        try:
+            database.disable_sensor_station(adr)
+        except DatabaseError as e:
+            log.error(f'Unable to disable sensor station {adr} in database: {e}')
+            continue
+
+def update_limits(sensor_station_info: list[dict]) -> None:
+    """Updates the sensor station limits with the given info"""
+    database = Database(DB_FILENAME)
+    for st in sensor_station_info:
+        address = st.get('address')
+        sensors = st.get('sensors', [])
+        log.info(f'Updating limits for sensor station {address}')
+        for sensor in sensors:
+            # update limits for each sensor
+            log.info(f'Adjusting limits for sensor {sensor.get("sensor_name")}')
+            try:
+                database.update_limits(sensor_station_address=address, **sensor)
+            except DatabaseError as e:
+                log.error(f'Unable to update setting for sensor {sensor} on sensor station {address} in database: {e}')   
 
 def get_config(config: Config) -> None:
     """
@@ -36,66 +72,39 @@ def get_config(config: Config) -> None:
                 log.info(f'No token received')
         except ConnectionError as e:
             log.error(e)
-            return
+        return
     
     # get new configuration
-    else:
-        log.info('Checking backend for new configuration')
-        try:
-            new_config_data, sensor_stations = backend.get_config()
-        except TokenDeclinedError:
-            log.warning('The sensor station has been locked by backend')
-            config.reset_token()
-            return
-        except ConnectionError as e:
-            log.error(e)
-            return
-        log.info('Received configuration')
+    log.info('Checking backend for new configuration')
+    try:
+        new_config_data, sensor_stations = backend.get_config()
+    except TokenDeclinedError:
+        log.warning('The sensor station has been locked by backend')
+        config.reset_token()
+        return
+    except ConnectionError as e:
+        log.error(e)
+        return
+    log.info('Received configuration')
 
-        # update configuration
-        scan_active_before = config.scan_active
-        config.update(**new_config_data)
-        if not scan_active_before and config.scan_active:
-            log.info('Enabling scan for new sensor stations')
+    # update configuration
+    scan_active_before = config.scan_active
+    config.update(**new_config_data)
+    if not scan_active_before and config.scan_active:
+        log.info('Enabling scan for new sensor stations')
 
-        # enable/disable sensor stations
-        try:
-            known_sensor_station_addresses = database.get_all_known_sensor_station_addresses()
-        except DatabaseError as e:
-            log.error(f'Unable to load addresses of known sensor stations from database: {e}')
-            return
-        sensor_stations_to_enable = [station.get('address')
-                                     for station in sensor_stations
-                                     if station.get('address') not in known_sensor_station_addresses]
-        sensor_stations_to_disable = [adr
-                                      for adr in known_sensor_station_addresses
-                                      if adr not in [station.get('address') for station in sensor_stations]]
-        if sensor_stations_to_enable: log.info(f'Enabling sensor stations: {sensor_stations_to_enable}')
-        for adr in sensor_stations_to_enable:
-            try:
-                database.enable_sensor_station(adr)
-            except DatabaseError as e:
-                log.error(f'Unable to enable sensor station {adr} in database: {e}')
-                continue
-        if sensor_stations_to_disable: log.info(f'Disabling sensor stations: {sensor_stations_to_disable}')
-        for adr in sensor_stations_to_disable:
-            try:
-                database.disable_sensor_station(adr)
-            except DatabaseError as e:
-                log.error(f'Unable to disable sensor station {adr} in database: {e}')
-                continue
+    # enable/disable sensor stations
+    try:
+        known_sensor_station_addresses = database.get_all_known_sensor_station_addresses()
+    except DatabaseError as e:
+        log.error(f'Unable to load addresses of known sensor stations from database: {e}')
+        return
+    enable_sensor_stations([station.get('address')
+                            for station in sensor_stations
+                            if station.get('address') not in known_sensor_station_addresses])
+    disable_sensor_stations([adr
+                                for adr in known_sensor_station_addresses
+                                if adr not in [station.get('address') for station in sensor_stations]])
 
-        # update limits for sensors if applicable
-        for sensor_station in sensor_stations:
-            address = sensor_station.get('address')
-            sensors = sensor_station.get('sensors')
-            if sensors:
-                log.info(f'Updating limits for sensor station {address}')
-                for sensor in sensors:
-                    # update limits for each sensor
-                    log.info(f'Adjusting limits for sensor {sensor.get("sensor_name")}')
-                    try:
-                        database.update_limits(sensor_station_address=address,
-                                                       **sensor)
-                    except DatabaseError as e:
-                        log.error(f'Unable to update setting for sensor {sensor} on sensor station {adr} in database: {e}')   
+    # update limits
+    update_limits(sensor_stations)
