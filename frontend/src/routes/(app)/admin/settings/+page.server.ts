2612eb0034f2428c0bd77e72814e74825ce837cf
Plunser Fabio <fabio.plunser@student.uibk.ac.at>
2023-05-01 22:04:22 +0200

    changed backendUrl to make better use of .env

--- frontend/src/routes/(app)/admin/settings/+page.server.ts
+++ frontend/src/routes/(app)/admin/settings/+page.server.ts
@@ -1,7 +1,7 @@
 import { BACKEND_URL } from "$env/static/private";
 
 export async function load({ fetch }) {
-  let res = await fetch(`http://${BACKEND_URL}/get-access-points`);
+  let res = await fetch(`${BACKEND_URL}/get-access-points`);
   res = await res.json();
 
   console.log("access points", res);
