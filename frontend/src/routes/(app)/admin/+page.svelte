1c24e9abfd1e4116064d7cceac551cff8551fe80
Plunser Fabio <fabio.plunser@student.uibk.ac.at>
2023-05-03 19:52:35 +0200

    changed admin nav to use bootstrap icons

--- frontend/src/routes/(app)/admin/+page.svelte
+++ frontend/src/routes/(app)/admin/+page.svelte
@@ -37,35 +37,4 @@
   ];
 </script>
 
-{#if rendered}
-  <div
-    class="flex justify-center gap-6 mt-12"
-    in:fly={{ y: -200, duration: 400 }}
-  >
-    {#each infoBadges as badges}
-      <div
-        class="relative rounded-full border-2 dark:border-none bg-base-100 drop-shadow-xl p-8"
-      >
-        <div class="absolute top-1 left-[16px] m-auto">
-          <svelte:component
-            this={badges.icon}
-            width={badges.size}
-            height={badges.size}
-            class="dark:fill-white rounded-full flex items-center justify-center "
-          />
-          <h1 class="flex justify-center rounded-full m-0 p-0">
-            {badges.number}
-          </h1>
-        </div>
-      </div>
-    {/each}
-  </div>
-
-  <div class="mt-20">
-    {#each { length: 3 } as _, i}
-      <div class="mt-4">
-        <Graph data={null} />
-      </div>
-    {/each}
-  </div>
-{/if}
+<!-- {#if rendered} <div class="flex justify-center gap-6 mt-12" in:fly={{ y: -200, duration: 400 }} > {#each infoBadges as badges} <div class="relative rounded-full border-2 dark:border-none bg-base-100 drop-shadow-xl p-8" > <div class="absolute top-1 left-[16px] m-auto"> <svelte:component this={badges.icon} width={badges.size} height={badges.size} class="dark:fill-white rounded-full flex items-center justify-center " /> <h1 class="flex justify-center rounded-full m-0 p-0"> {badges.number} </h1> </div> </div> {/each} </div> <div class="mt-20"> {#each { length: 3 } as _, i} <div class="mt-4"> <Graph data={null} /> </div> {/each} </div> {/if} -->
