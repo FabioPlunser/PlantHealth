6bdf416eee44a5232e2f219d1df44d0515474284
Plunser Fabio <fabio.plunser@student.uibk.ac.at>
2023-05-09 10:57:38 +0200

    updated some responisve to show sensorstation locked and deleted in frontend

--- backend/src/main/java/at/ac/uibk/plant_health/models/rest_responses/DashBoardDataResponse.java
+++ backend/src/main/java/at/ac/uibk/plant_health/models/rest_responses/DashBoardDataResponse.java
@@ -34,52 +34,31 @@ public class DashBoardDataResponse extends RestResponse implements Serializable 
 		private final String roomName;
 		private final UUID sensorStationId;
 		private final List<UUID> pictureIds;
-
-		//		private final List<TimeStampedSensorData> values;
+		private final boolean connected;
+		private final boolean unlocked;
+		private final boolean deleted;
 
 		public DashboardSensorStation(SensorStation sensorStation) {
-			this.name = sensorStation.getName();
-
-			this.roomName = sensorStation.getAccessPoint().getRoomName();
-			this.pictureIds = sensorStation.getPlantPictures()
-									  .stream()
-									  .map(SensorStationPicture::getPictureId)
-									  .toList();
-			this.sensorStationId = sensorStation.getDeviceId();
-
-			//			this.values = sensorStation.getSensorData()
-			//					.stream()
-			//					.collect(groupingBy(SensorData::getTimeStamp))
-			//					.entrySet()
-			//					.stream()
-			//					.map(TimeStampedSensorData::new)
-			//					.sorted(Comparator.comparing(o -> o.timestamp))
-			//					.toList();
+			if (!sensorStation.isDeleted()) {
+				this.name = sensorStation.getName();
+				this.roomName = sensorStation.getAccessPoint().getRoomName();
+				this.pictureIds = sensorStation.getPlantPictures()
+										  .stream()
+										  .map(SensorStationPicture::getPictureId)
+										  .toList();
+				this.sensorStationId = sensorStation.getDeviceId();
+				this.connected = sensorStation.isConnected();
+				this.unlocked = sensorStation.isUnlocked();
+				this.deleted = false;
+			} else {
+				this.name = null;
+				this.roomName = null;
+				this.pictureIds = null;
+				this.sensorStationId = null;
+				this.connected = false;
+				this.unlocked = false;
+				this.deleted = true;
+			}
 		}
 	}
-
-	//	@Getter
-	//	private class TimeStampedSensorData implements Serializable {
-	//		private final LocalDateTime timestamp;
-	//		private final List<InnerSensorData> sensors;
-	//		public TimeStampedSensorData(Map.Entry<LocalDateTime, List<SensorData>> e) {
-	//			this.timestamp = e.getKey();
-	//			this.sensors = e.getValue().stream().map(InnerSensorData::new).toList();
-	//		}
-	//	}
-	//
-	//	@Getter
-	//	private class InnerSensorData implements Serializable {
-	//		private final String sensor;
-	//		private final double value;
-	//		private final String unit;
-	//		private final char alarm;
-	//
-	//		public InnerSensorData(SensorData sensorData) {
-	//			this.sensor = sensorData.getSensor().getType();
-	//			this.value = sensorData.getValue();
-	//			this.unit = sensorData.getSensor().getUnit();
-	//			this.alarm = sensorData.getAlarm();
-	//		}
-	//	}
 }
