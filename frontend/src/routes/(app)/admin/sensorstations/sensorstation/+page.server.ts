5e20301ed8ca159c43e6a8af45b7ba1635236d70
Emanuel Prader <emanuel.prader@student.uibk.ac.at>
2023-05-09 23:51:57 +0200

    initial update action

--- frontend/src/routes/(app)/admin/sensorstations/sensorstation/+page.server.ts
+++ frontend/src/routes/(app)/admin/sensorstations/sensorstation/+page.server.ts
@@ -1,5 +1,6 @@
 import { BACKEND_URL } from "$env/static/private";
 import { error } from "@sveltejs/kit";
+import { z } from "zod";
 
 export async function load({ fetch, depends, url, cookies }) {
   let sensorStationId = cookies.get("sensorStationId");
@@ -12,6 +13,16 @@ export async function load({ fetch, depends, url, cookies }) {
   depends("app:getSensorStation");
   return res;
 }
+
+const schema = z.object({
+  name: z
+    .string({ required_error: "Name is required" })
+    .min(1, { message: "Name is required" })
+    .min(6, { message: "Name must be at least 6 characters" })
+    .max(32, { message: "Name must be less than 32 characters" })
+    .trim()
+    .or(z.literal("")),
+});
 
 export const actions = {
   // TODO: add validation and error handling (toast messages)
@@ -37,7 +48,22 @@ export const actions = {
     });
   },
 
-  update: async ({ cookies, request, fetch }) => {},
+  update: async ({ cookies, request, fetch }) => {
+    const formData = await request.formData();
+    const zodData = schema.safeParse(Object.fromEntries(formData));
+
+    if (!zodData.success) {
+      // Loop through the errors array and create a custom errors array
+      const errors = zodData.error.errors.map((error) => {
+        return {
+          field: error.path[0],
+          message: error.message,
+        };
+      });
+
+      return fail(400, { error: true, errors });
+    }
+  },
 
   setLimits: async ({ cookies, request, fetch }) => {
     let formData = await request.formData();
