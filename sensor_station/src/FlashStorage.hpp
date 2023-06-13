4162cb5a73f0917dd02b43d5e25b5b458dbd7820
Lukas Kirchmair <csaw9392@student.uibk.ac.at>
2023-06-13 20:36:12 +0200

    Added debug prints.

--- sensor_station/src/FlashStorage.hpp
+++ sensor_station/src/FlashStorage.hpp
@@ -105,12 +105,15 @@ class FlashStorage {
 			// }
 			DEBUG_PRINTF(2, "Read %s from flash.\n", (char *) ramBuffer);
 			arduino::String pairedDevice;
+			Serial.println("Size Bytes = %lu\n", PAIRED_DEVICE_BLOCK.sizeBytes);
 			for (uint32_t i = 0; i < PAIRED_DEVICE_BLOCK.sizeBytes; i++) {
 				if (ramBuffer[i] == 0) {
 					break;
 				}
 				pairedDevice += ramBuffer[i];
 			}
+			Serial.println("Paired Device = %s\n", pairedDevice.c_str());
+			delete[] ramBuffer;
 			return pairedDevice;
 		}
 
